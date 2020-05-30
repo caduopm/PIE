38ec1f791c4c24db8f8d583daed1be0c0e5ff2b7
cadu <50914040+caduopm@users.noreply.github.com>
2020-05-30 16:11:38 -0300

    teste merge (#3)
    * teste
    
    * config banco de dados
    
    * config urls - static files - reutilizar templates
    
    Co-authored-by: Nubi4 <nubialuanadeoculos@gmail.com>
    Co-authored-by: cesouza <cesouza@ibm.com>

---
+++ ProjetoIntegradoE/gerenciador_htoh/urls.py
@@ -0,0 +1,27 @@
+"""gerenciador_htoh URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app_htoh.urls')),
+    path('login/', include('app_login.urls')),
+    path('adopt/', include('app_adopt.urls')),
+    path('chart/', include('app_chart.urls')),
+    path('donate/', include('app_donate.urls')),
+    path('find/', include('app_find.urls')),
+]
