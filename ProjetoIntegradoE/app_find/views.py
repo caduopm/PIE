0b36f372463feba7640bec9ec2c9669cde14b550
cesouza <cesouza@ibm.com>
2020-06-03 21:59:21 -0300

    apresentando dados do BD na tela

--- ProjetoIntegradoE/app_find/views.py
+++ ProjetoIntegradoE/app_find/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
+from django.apps import apps
+Animal = apps.get_model('app_animal', 'Animal')
 
 
 def find(request):
-    return render(request, 'find.html')
+    data = {}
+    data['animals'] = Animal.objects.all()
+    return render(request, 'find.html', data)
